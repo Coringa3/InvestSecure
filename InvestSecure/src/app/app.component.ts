a069d460bf548f14ca4659e81f07047907d84bd9
Coringa73 <96151890+Coringa73@users.noreply.github.com>
2024-06-19 11:00:52 +0100

    Teste de pc

--- InvestSecure/src/app/app.component.ts
+++ InvestSecure/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FinnhubService } from './finnhub.service';
 
 @Component({
-  selector: 'pm-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'InvestSecure';
+export class AppComponent implements OnInit {
+  title = 'finnhub-app';
+  stockQuote: any;
+  companyProfile: any;
+
+  constructor(private finnhubService: FinnhubService) {}
+
+  ngOnInit() {
+    this.getStockQuote('AAPL'); // Exemplo com o símbolo da Apple
+    this.getCompanyProfile('AAPL');
+  }
+
+  getStockQuote(symbol: string) {
+    this.finnhubService.getStockQuote(symbol).subscribe(
+      data => {
+        this.stockQuote = data;
+        console.log(this.stockQuote);
+      },
+      error => {
+        console.error('Error fetching stock quote', error);
+      }
+    );
+  }
+
+  getCompanyProfile(symbol: string) {
+    this.finnhubService.getCompanyProfile(symbol).subscribe(
+      data => {
+        this.companyProfile = data;
+        console.log(this.companyProfile);
+      },
+      error => {
+        console.error('Error fetching company profile', error);
+      }
+    );
+  }
 }
--- InvestSecure/src/app/app.module.ts
+++ InvestSecure/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FinnhubService } from './finnhub.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
-  providers: [],
+  providers: [FinnhubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
