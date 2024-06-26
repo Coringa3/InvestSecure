c8dc00a568828948d9f555f882397fe92ade24e2
Coringa <diogomes005@gmail.com>
2024-06-26 19:16:51 +0100

    add NVDIA

--- InvestSecure/src/app/random-stocks/random-stocks.component.ts
+++ InvestSecure/src/app/random-stocks/random-stocks.component.ts
@@ -9,7 +9,7 @@ import { FinnhubService } from '../finnhub.service';
 export class StockDisplayComponent implements OnInit {
   stockQuotes: any[] = [];
   companyProfiles: any[] = [];
-  symbols: string[] = ['AAPL', 'GOOGL', 'TSLA', 'AMZN', 'MSFT'];
+  symbols: string[] = ['AAPL', 'GOOGL', 'TSLA', 'AMZN', 'MSFT', 'NVDA'];
 
   constructor(private finnhubService: FinnhubService) { }
 
